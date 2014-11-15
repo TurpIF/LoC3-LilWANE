71f143dc11145d7b1ae97717b5a705acc076846c
TurpIF <pierre.turpin@insa-lyon.fr>
2014-11-15 10:53:08 +0100

    Ajout du main

---
+++ src/main/java/fr/lilwane/bot/Main.java
@@ -0,0 +1,75 @@
+package main.java.fr.lilwane.bot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.d2si.loc.api.Bot;
+import com.d2si.loc.api.datas.Board;
+import com.d2si.loc.api.datas.Castle;
+import com.d2si.loc.api.datas.Troop;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Created by turpif on 15/11/14.
+ */
+public class Main implements Bot {
+    private static final Logger log = LogManager.getLogger(Main.class);
+
+    private Random r = new Random();
+
+    @Override
+    public void init(Board board) {
+        // Special initializations (if needed)
+        log.info("init: " + board);
+    }
+
+    @Override
+    public void onTroopError(int turnNumber, String messageError, Troop troopInError) {
+        // A previously troop sent was illegal...
+        log.info("onTroopError: turnNumber[" + turnNumber + "] msg[" + messageError + "] troop:" + troopInError);
+    }
+
+    @Override
+    public void onGameEnded(boolean areYouWinner, Board board) {
+        // Am I winner?
+        log.info("onGameEnded: winner[" + areYouWinner + "]");
+    }
+
+    @Override
+    public List<Troop> onNewTurn(Board board) {
+        // Decide what troops to send on this new turn
+        List<Troop> troopsToSendOnThisTurn = new ArrayList<>();
+
+        // Send units on each castles I own
+        List<Castle> myCastles = board.getMineCastles();
+        for (Castle castle : myCastles) {
+            int maxUnitToSend = castle.getUnitCount() - 1; // Let at least one unit on this castle!
+            if (maxUnitToSend <= 1) {
+                // Nothing to send on this castle
+                continue;
+            }
+
+            // Compute random unit to send
+            int unitToSend = r.nextInt(maxUnitToSend) + 1;
+
+            // Find a random destination castle
+            List<Castle> enemyCastles = board.getOpponentsCastles();
+            if (enemyCastles.isEmpty()) {
+                continue;
+            }
+
+            int castleIndex = r.nextInt(enemyCastles.size());
+            Castle destination = enemyCastles.get(castleIndex);
+
+            // Finally create troop object
+            Troop troop = new Troop(destination, castle, unitToSend);
+            troopsToSendOnThisTurn.add(troop);
+        }
+
+        // Return all new troops
+        return troopsToSendOnThisTurn;
+    }
+}
